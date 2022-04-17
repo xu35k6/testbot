52f6d3309cabe18eabbc92a1d631846b1ab1efcb
xu35k6 <93394491+xu35k6@users.noreply.github.com>
2022-04-17 20:16:12 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,15 @@
 # -*- coding: utf-8 -*-
 """
 Created on Wed Jun  2 21:16:35 2021
-
 @author: Ivan
 版權屬於「行銷搬進大程式」所有，若有疑問，可聯絡ivanyang0606@gmail.com
 
 Line Bot聊天機器人
-第三章 互動回傳功能
-推播push_message與回覆reply_message
+第一章 Line Bot申請與串接
+Line Bot機器人串接與測試
 """
 #載入LineBot所需要的套件
 from flask import Flask, request, abort
-import win32api
 
 from linebot import (
     LineBotApi, WebhookHandler
@@ -20,15 +18,16 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import *
-import re
+
 app = Flask(__name__)
 
 # 必須放上自己的Channel Access Token
-line_bot_api = LineBotApi('CPhgJYcjjNif05kQQkgImPDFZ/2tS4ri9mSxtWoayNAVw8cYKaOAGyUM5WYQyCeB4/8oB4qkcD/hfnJVeZPyQOkISkwmR0Z8eVDv2cMX8zTtlZP1gKONeXB3VSEeTirUPClq4zdGOOLGIgRKHAwbaQdB04t89/1O/w1cDnyilFU=')
+line_bot_api = LineBotApi('你自己的token')
 # 必須放上自己的Channel Secret
-handler = WebhookHandler('605838c73d96732cf0a43fe63ca4c236')
+handler = WebhookHandler('你自己的secret')
 
-line_bot_api.push_message('Ucc6518f109ac9b19bc3f619594ecd2fb', TextSendMessage(text='你可以開始了'))
+line_bot_api.push_message('你自己的ID', TextSendMessage(text='你可以開始了'))
+
 
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
@@ -36,6 +35,7 @@ def callback():
     # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
 
+ 
     # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
@@ -48,15 +48,14 @@ def callback():
 
     return 'OK'
 
+ 
 #訊息傳遞區塊
 ##### 基本上程式編輯都在這個function #####
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = text=event.message.text
-    if re.match('告訴我秘密',message):
-        line_bot_api.reply_message(event.reply_token,TextSendMessage('才不告訴你哩！'))
-    else:
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(message))
+    message = TextSendMessage(text=event.message.text)
+    line_bot_api.reply_message(event.reply_token,message)
+
 #主程式
 import os
 if __name__ == "__main__":
